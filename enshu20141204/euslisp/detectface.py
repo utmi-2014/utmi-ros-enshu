32c02b7743d7e8c35ec442f362553fa634b9cab8
楊天任 <yangtianren@tenys-MacBook-Pro.local>
2014-12-04 20:28:10 +0900

    i made beautiful indent

--- enshu20141204/euslisp/detectface.py
+++ enshu20141204/euslisp/detectface.py
@@ -11,66 +11,58 @@ import numpy as np
 
 
 class DetectFace:
-	def __init__(self):
-		rospy.init_node('listener', anonymous=True)
-		self.pub = rospy.Publisher('/enshu/detectface', Bool)
-		rospy.Subscriber('/camera/rgb/image_raw/compressed', CompressedImage, self.callback)
+    def __init__(self):
+        rospy.init_node('listener', anonymous=True)
+            self.pub = rospy.Publisher('/enshu/detectface', Bool)
+            rospy.Subscriber('/camera/rgb/image_raw/compressed', CompressedImage, self.callback)
+                self.detected = False
 
-		self.detected = False
+        def callback(self, data):
 
+            jpeg = data.data
+            byte_array = bytearray(jpeg)
+            file_bytes = np.array(byte_array)
+            image = cv2.imdecode(file_bytes, cv2.CV_LOAD_IMAGE_UNCHANGED)
 
-	def callback(self, data):
-		#rospy.loginfo(rospy.get_name() + ': I heard {}'.format(data.data))
-		#cv2.imread(data.data)
+            #cascade_path = "/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml"
+            cascade_path = "/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml"
+            #cascade_path = "/usr/share/opencv/haarcascades/haarcascade_frontalface_alt2.xml"
+            #cascade_path = "/usr/share/opencv/haarcascades/haarcascade_frontalface_alt_tree.xml"
 
-		jpeg = data.data
-		byte_array = bytearray(jpeg)
-		file_bytes = np.array(byte_array)
-		image = cv2.imdecode(file_bytes, cv2.CV_LOAD_IMAGE_UNCHANGED)
+            color = (255, 255, 255) #白
 
-		#cascade_path = "/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml"
-		cascade_path = "/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml"
-		#cascade_path = "/usr/share/opencv/haarcascades/haarcascade_frontalface_alt2.xml"
-		#cascade_path = "/usr/share/opencv/haarcascades/haarcascade_frontalface_alt_tree.xml"
+            #グレースケール変換
+            #image_gray = cv2.cvtColor(image, cv2.cv.CV_BGR2GRAY)
 
+            #カスケード分類器の特徴量を取得する
+            cascade = cv2.CascadeClassifier(cascade_path)
 
-		#image_path = "data.data"
+            #facerect = cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
+            facerect = cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=3, minSize=(10, 10), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
 
-		color = (255, 255, 255) #白
-		#color = (0, 0, 0) #黒
+            print facerect
 
-		#グレースケール変換
-		#image_gray = cv2.cvtColor(image, cv2.cv.CV_BGR2GRAY)
+            if len(facerect) <= 0:
+                self.detected = False
+                    return
 
-		#カスケード分類器の特徴量を取得する
-		cascade = cv2.CascadeClassifier(cascade_path)
+            #検出した顔を囲む矩形の作成
+            for rect in facerect:
+                cv2.rectangle(image, tuple(rect[0:2]),tuple(rect[0:2]+rect[2:4]), color, thickness=2)
 
-		#facerect = cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
-		facerect = cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=3, minSize=(10, 10), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
+            #認識結果の保存
 
-		print facerect
-
-		if len(facerect) <= 0:
-			self.detected = False
-			return
-		
-		#検出した顔を囲む矩形の作成
-		for rect in facerect:
-			cv2.rectangle(image, tuple(rect[0:2]),tuple(rect[0:2]+rect[2:4]), color, thickness=2)
-
-		#認識結果の保存
-
-		cv2.imwrite("./test.jpeg",image)
-		print "face detected!"
-		self.detected = True
+            #cv2.imwrite("./test.jpeg",image)
+            print "face detected!"
+            self.detected = True
 
 
 def main():
-	detect_face = DetectFace()
-	while not rospy.is_shutdown():
-		detect_face.pub.publish(Bool(detect_face.detected))
- 		rospy.sleep(1.)
+    detect_face = DetectFace()
+        while not rospy.is_shutdown():
+            ish(Bool(detect_face.detected))
+                rospy.sleep(1.)
 
 
 if __name__ == '__main__':
-	main()
+    main()
