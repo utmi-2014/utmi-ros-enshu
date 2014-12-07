5261bbf0a9b7a670a3b600c765f51e3e7f392971
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-12-07 17:34:38 +0900

    improve: test with selecting action

--- enshu20141204/scripts/group15/get_cmdline_input.py
+++ enshu20141204/scripts/group15/get_cmdline_input.py
@@ -5,12 +5,19 @@
 import rospy
 from std_msgs.msg import String
 
+import sys
 
 def get_cmdline_input():
     pub = rospy.Publisher('/enshu/get_cmdline_input', String)
     rospy.init_node('get_cmdline_input')
 
-    data = raw_input("email: ")
+    if sys.argv[1] == 'email':
+        data = raw_input("email: ")
+    elif sys.argv[1] == 'select':
+        print "何をしてほしいですか？"
+        print "1: 写真を撮らせる"
+        print "2: お酌させる"
+        data = raw_input("select: ")
     # rospy.loginfo(data)
     pub.publish(String(data))
 
--- enshu20141204/scripts/group15/ytk_test.py
+++ enshu20141204/scripts/group15/ytk_test.py
@@ -26,7 +26,7 @@ class YtkDemo(object):
         rospy.Subscriber('/enshu/get_cmdline_input', String, self.cb_get_cmdline_input)
 
         self.start_demo = False
-        self.email = ''
+        self.cmdline_input = ''
         self.exist_man_5frame = [False] * 5
 
     def cb_detect_face(self, data):
@@ -35,46 +35,52 @@ class YtkDemo(object):
         self.exist_man_5frame.append(data.data)
 
     def cb_get_cmdline_input(self, data):
-        self.email = data.data
+        self.cmdline_input = data.data
 
     def test(self):
         rospy.sleep(2)
         self.pub_speak_robot.publish(String('こんにちは。'))
         rospy.sleep(4)
-        self.pub_speak_robot.publish(String('写真を撮りませんか？'))
+        # select
+        self.pub_speak_robot.publish(String('何をしますか？'))
         rospy.sleep(4)
-        self.pub_speak_robot.publish(String('三・・二・・一'))
-        rospy.sleep(4)
-        self.pub_speak_robot.publish(String('カシャッ！'))
-        rospy.sleep(1)
-        self.pub_take_photo.publish()
-        rospy.sleep(2)
-        self.pub_speak_robot.publish(String('写真を撮りました。・・'
-                                            'メールで送ることができますので、・・'
-                                            'メールアドレスを打ち込んでください。'))
-        rospy.sleep(11)
-
-        # input email
         cmd = ('gnome-terminal -e "python /home/wken/catkin_ws/enshu/src/'
-               'utmech-ros-enshu/enshu20141204/scripts/group15/get_cmdline_input.py"')
+               'utmech-ros-enshu/enshu20141204/scripts/group15/get_cmdline_input.py select"')
         os.system(cmd)
 
-        # send mail
-        body = ('上手く撮れていますか？\nまた, 私のプログラムはここにありますので、見てみてください。\n'
-                'https://github.com/wkentaro/utmech-ros-enshu/enshu20141204/scripts/group15/ytk_demo.py\n')
-        subject = 'ytk_demo.py: Take Photo'
-        attachment = '/tmp/ytk_take_photo.jpeg'
-        cmd = 'echo "{0}" | mutt -s "{1}" -a {2} -- {3}'
-        cmd = cmd.format(body, subject, attachment, self.email)
-        os.system(cmd)
-        # say user to check the inbox
-        self.pub_speak_robot.publish(String('メールを送りました。・・受信箱を確認してください。'))
-        rospy.sleep(7)
-
-        # pass drink
-        self.pub_speak_robot.publish(String('お茶でもどうですか？'))
-        rospy.sleep(7)
-        self.pub_catch_drink.publish()
+        if self.cmdline_input == '1':
+            self.pub_speak_robot.publish(String('写真を撮ります！'))
+            rospy.sleep(4)
+            self.pub_speak_robot.publish(String('三・・二・・一'))
+            rospy.sleep(4)
+            self.pub_speak_robot.publish(String('カシャッ！'))
+            rospy.sleep(1)
+            self.pub_take_photo.publish()
+            rospy.sleep(2)
+            self.pub_speak_robot.publish(String('写真を撮りました。・・'
+                                                'メールで送ることができますので、・・'
+                                                'メールアドレスを打ち込んでください。'))
+            rospy.sleep(11)
+            # input email
+            cmd = ('gnome-terminal -e "python /home/wken/catkin_ws/enshu/src/'
+                'utmech-ros-enshu/enshu20141204/scripts/group15/get_cmdline_input.py email"')
+            os.system(cmd)
+            # send mail
+            body = ('上手く撮れていますか？\nまた, 私のプログラムはここにありますので、見てみてください。\n'
+                    'https://github.com/wkentaro/utmech-ros-enshu/enshu20141204/scripts/group15/ytk_demo.py\n')
+            subject = 'ytk_demo.py: Take Photo'
+            attachment = '/tmp/ytk_take_photo.jpeg'
+            cmd = 'echo "{0}" | mutt -s "{1}" -a {2} -- {3}'
+            cmd = cmd.format(body, subject, attachment, self.cmdline_input)
+            os.system(cmd)
+            # say user to check the inbox
+            self.pub_speak_robot.publish(String('メールを送りました。・・受信箱を確認してください。'))
+            rospy.sleep(7)
+        elif self.cmdline_input == '2':
+            # pass drink
+            self.pub_speak_robot.publish(String('お茶でもどうですか？'))
+            rospy.sleep(7)
+            self.pub_catch_drink.publish()
 
 
 if __name__ == '__main__':
